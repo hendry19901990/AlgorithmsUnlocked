827482e38447fee7c996fd246b1b7fd695d7b3d5
SpawnTree <shekharhiran@hotmail.com>
2018-02-11 21:31:33 +0530

    fceab5923438450f8e641be3ddbd4c2b1a2df619

--- Source_Files/source.cpp
+++ Source_Files/source.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include "source.h"
+char s[] = "This a great day for us cause we won the battle.";
 int main()
 {   
+    std::cout << getstringlenth(s) << sizeof(s);
     return 0;
 }
