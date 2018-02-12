9d3e41dab1e81de1627649d89ed51c91e5f21373
SpawnTree <shekharhiran@hotmail.com>
2018-02-12 17:19:26 +0530

    Pointers

--- Source_Files/source.cpp
+++ Source_Files/source.cpp
@@ -21,15 +21,39 @@ void getarrayofpointers(char **array)
     printf("%s\n",*++p+1);
     printf("%s\n",++*p+2);
     printf("%s\n",*p++);
+    printf("%s\n",p);
 }
 char** passvalues()
 {
-
+    char **p = (char**)malloc(  sizeof(char*)*MAX_LIMIT_SZ );
+    for(int i = 0; i < MAX_LIMIT_SZ; i++)
+    {
+        p[i] = (char*)malloc(MAX_LIMIT_SZ);
+    }
+    char *arr[] = {
+    "This is a tool",
+    "is a Tool",
+    "a pool",
+    "pointer for",
+    "to get",
+    "an what",
+    "array of what",
+    "Done nothing"
+  };
+    for(int i = 0; i < MAX_LIMIT_SZ; i++)
+    {
+        p[i] = arr[i];
+    }
+  return p;
 }
-char *arr = (char*)malloc(MAX_LIMIT_SZ);
 int main(int argc, char const **argv)
 {   
     char **array = (char**)malloc(MAX_LIMIT_SZ*sizeof(char*));
+    for(int i = 0; i < MAX_LIMIT_SZ; i++)
+    {
+        array[i] = (char*)malloc(MAX_LIMIT_SZ);
+    }
+    char *arr = (char*)malloc(MAX_LIMIT_SZ);
     array = s;
     if(arr == NULL || array == NULL)
     {
@@ -38,5 +62,7 @@ int main(int argc, char const **argv)
     }
     fgets(arr, MAX_LIMIT_SZ, stdin);
     getarrayofpointers(array);
+    free(array);
+    free(arr);
     return 0;
 }
