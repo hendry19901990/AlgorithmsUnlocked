3a5b82002e3461700070299959a3211ab581fc9a
SpawnTree <shekharhiran@hotmail.com>
2018-02-12 14:46:12 +0530

    sh

--- Source_Files/start.cpp
+++ Source_Files/start.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 #include "start.h"
-int getfunc();
+char *string = "_INOVKE_INITALIZER & _TRAILS";
+char *s[] = {
+    "This",
+    "is",
+    "a",
+    "pointer",
+    "to",
+    "an",
+    "array",
+    "Done"
+};
+char manipulate(char **s)
+{
+    printf("%s\n",s);
+}
 
 
