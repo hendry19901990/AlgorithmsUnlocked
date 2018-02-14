f08ba925f14cf5d74d1a7bbb20f10aee8cceed1e
SpawnTree <shekharhiran@hotmail.com>
2018-02-14 15:59:15 +0530

    Node

--- libjscript/http.js
+++ libjscript/http.js
@@ -1,13 +1,15 @@
 var http = require('http');
 var url = require('url');
-try{
-    var server = http.createServer(function(req,res){
-    res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200, {'Content-Type':'application/json'});
-    var urlData = url.parse(req.url,true).query;
-    res.write(JSON.stringify(urlData,null,3));
-    res.end();
-    console.log("Running  Server");
-}).listen(8020)}catch(err){
-    console.log(err);
+var port = 8056;
+var hostname = '127.0.0.1';
+var server = http.createServer(server_engine);
+function server_engine(request,response,error){
+    response.setHeader('Content-Type','application/json');
+    response.writeHead(200,{'Content-Type':'application/json'});
+    var urlData = url.parse(request.url,true).query;
+    response.write(JSON.stringify(urlData,null,3));
+    response.end('\n' + "Server response ended");
 }
+server.listen(port,hostname,function(){
+    console.log("Server Started and ready : http://" + hostname + ":" + port);
+});
