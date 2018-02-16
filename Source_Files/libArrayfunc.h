22c8c1f7ec29aa2d6a344435667e34b18f51c3e3
SpawnTree <shekharhiran@hotmail.com>
2018-02-16 23:25:05 +0530

    Templates & C++

--- Source_Files/libArrayfunc.h
+++ Source_Files/libArrayfunc.h
@@ -4,6 +4,20 @@
 #include <cstdio>
 #define MAX_LIMIT_SZ 2048
 using namespace std;
+template<typename T,size_t size, char c> class Arraytype{
+    T array[size];
+public: 
+    T& operator[](size_t t);
+    void* operator new(size_t p);
+    Arraytype operator()(char j,int p);
+    T& operator+(int i);
+    T& operator<<(int j);
+};
+template<typename T,size_t size, char c>
+T& Arraytype<T,size,c> :: operator[](size_t p)
+{
+
+}
 int runMethodArrays()
 {
     return 0;
