c48f350166b1e45f85b728a1e4f7ecff5d8b3823
SpawnTree <shekharhiran@hotmail.com>
2018-02-18 23:23:13 +0530

    Final Commit

--- Source_Files/objectsrc/array_class.cpp
+++ Source_Files/objectsrc/array_class.cpp
@@ -6,34 +6,5 @@
 #include "libarrayclass.hpp"
 int main(int argc, char** argv)
 {
-  /* Size Type*/
-  size_t size;
-  /* Generic Allocation */
-  _Carray<int> object;
-  /* Assignment */
-  _Carray<char**> array1(size), array2;
-  array1 = array2; /* Assignment Operator Called */
-  /* Allocating from the heap one object only */
-  _Carray<double**> *doublearray = new _Carray<double**>(size);
-  /* Allocating from the heap an array of objects */
-  _Carray<unsigned int> *u_arrayobj = new _Carray<unsigned int> [3];
-  /* Calling copy constructor */
-  _Carray<char**> chararray = array1; /* Copy Constructor Called */
-  /* Parameterized Constructor Called */
-  _Carray<unsigned int**> *int_array = new _Carray<unsigned int**>(size); 
-  /* Extractors */
-  std::cout << object << array1 << array2 << chararray;
-  /* Array Access */
-  u_arrayobj[0][0] = 50;
-  std::cout << u_arrayobj[0][0] << "\n";
-  /* Istream*/
-  std::cin >> object >> array1 >> array2 >> chararray;
-  /* Functor Called */
-  array1(array1,8);
-  /* Deleting from heaps */
-  delete int_array; /* Deleting single object, destructor is also called*/
-  delete[] u_arrayobj; /* Deleting array of objects, destructors are called */ 
-  delete doublearray; /* Deleting single object, destructor is also called*/
-  /* return */
   return 0;
 }
--- Source_Files/objectsrc/libarrayclass.hpp
+++ Source_Files/objectsrc/libarrayclass.hpp
@@ -1,10 +1,10 @@
 /* Copyright (c) 2017, Sumit Lahiri */
 
 /** 
- * The implamentation here is only dummy but it shows how to overload operators
+ * The implementation here is only dummy but it shows how to overload operators
  * in case of classes defined with a template typename. Hope this will clarify the 
  * basics of how to write such kinds of code and will also help in better understanding of 
- * Functors, overloading & Constructors 
+ * Functors, overloading & Constructors along with operator oveloading
  **/
 /*
 
@@ -33,13 +33,14 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef _LIBARRAYCLASS_H
 #define _LIBARRAYCLASS_H 1
-#endif
 
 #ifndef MAX_SIZE
 #define MAX_SIZE 8192
 #define BUFF_SIZE 1024
 #endif
+
 #define EXIT_FAILURE 1
+
 #ifndef NULL
 #undef NULL
 #define	NULL	0
@@ -57,18 +58,19 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include<string>
 #include<new>
 #include<vector>
-  typedef unsigned int _int_type;
-  typedef unsigned long _double_type;
-  typedef unsigned char _char_type;
-  typedef size_t _size_type;
-  typedef wchar_t _wide_type;
-  typedef const std::nothrow_t& _nothrow;
-  typedef std::istream& _input_stream;
-  typedef std::ostream& _output_stream;
-  typedef std::bad_alloc _bad_allocation;
-  typedef class object _object;
-  typedef void* _void_ptr;
-#ifndef _input_stream
+typedef unsigned int _int_type;
+typedef unsigned long _double_type;
+typedef unsigned char _char_type;
+typedef size_t _size_type;
+typedef wchar_t _wide_type;
+typedef const std::nothrow_t& _nothrow;
+typedef std::istream& _input_stream;
+typedef std::ostream& _output_stream;
+typedef std::bad_alloc _bad_allocation;
+typedef class object _object;
+typedef void* _void_ptr;
+#ifndef _BASEDEF_H
+#define _BASEDEF_H 1
 class __attribute__((__packed__)) Basedef{
 public:
   typedef unsigned int _int_type;
@@ -88,7 +90,7 @@ public:
 /* Template Class for Array entry & Array operations */
 template <typename Atype> class __attribute__((__packed__)) _Carray : public Basedef{
   /* Array */
-  Atype array[BUFF_SIZE];
+  Atype *array;
   /* Size */
   _size_type size;
   /* iterator */
@@ -98,43 +100,43 @@ template <typename Atype> class __attribute__((__packed__)) _Carray : public Bas
   /* static int */
   static int count;
 public:
-  static inline void getCount();
+  static inline void getCount(); /* To be implemented later */
   template <typename U> inline friend _input_stream operator>>(_input_stream,_Carray<U> const &) noexcept;
   /* Extractor Stream */
   template <typename U> inline friend _output_stream operator<<(_output_stream,_Carray<U> const &) noexcept;
   /* Buffer Filled Stream */
-  inline Atype &operator[](int) noexcept;
+  inline Atype& operator[](int) noexcept;
   /* Array Subscripter */
-  inline _Carray& operator++(int);
-  inline _Carray& operator--(int);
+  inline _Carray operator++(int);
+  inline _Carray operator--(int);
   /* Adding operation */
   inline _void_ptr operator new(_size_type) __attribute__((alloc_size(1),aligned(1)));
   inline _void_ptr operator new(_size_type, _nothrow) __attribute__((alloc_size(1),aligned(1)));
   /* New Allocation */
-  inline _void_ptr operator new[] (_size_type) __attribute__((alloc_size(1),aligned(1)));
-  inline _void_ptr operator new[] (_size_type, _nothrow) __attribute__((alloc_size(1),aligned(1)));
+  inline _void_ptr operator new[](_size_type) __attribute__((alloc_size(1),aligned(1)));
+  inline _void_ptr operator new[](_size_type, _nothrow) __attribute__((alloc_size(1),aligned(1)));
   /* New Allocation Object Array */
   inline void operator delete(_void_ptr);
   inline void operator delete(_void_ptr,_nothrow);
   /* Delete */
-  inline void operator delete[] (_void_ptr);
-  inline void operator delete[] (_void_ptr,_nothrow);
+  inline void operator delete[](_void_ptr);
+  inline void operator delete[](_void_ptr,_nothrow);
   /* =OP */
-  inline _Carray& operator=(_Carray&);
+  inline _Carray operator=(_Carray&);
   /* op() */
-  inline _Carray operator()(_Carray&, _int_type);
+  inline _Carray operator()(int);
   /* Add/Subs at a particular position */
   static inline void ObjectRefs(_object);
   /* Get Object Refs */
-  template<typename _compare> inline bool operator==(_compare);
+  template<typename _compare> inline bool operator==(_compare);  /* To be defined as per need */
   /*Compare == */
-  template<typename _compare> inline bool operator!=(_compare);
+  template<typename _compare> inline bool operator!=(_compare);  /* To be defined as per need */
   /* Compare != */
-  template<typename _compare> inline bool operator<(_compare);
+  template<typename _compare> inline bool operator<(_compare);  /* To be defined as per need */
   /* Compare < */
-  template<typename _compare> inline bool operator>(_compare);
+  template<typename _compare> inline bool operator>(_compare);  /* To be defined as per need */
   /* Compare > */
-  inline _Carray* operator->(){return *this;} __attribute__((alloc_size(1)));
+  inline _Carray* operator->(){return *this;} /* Overload () */
   /* Constructor */
   inline ~_Carray() noexcept; 
   /* Destructor */
@@ -143,151 +145,179 @@ public:
   /* Copy Constructor */
   inline _Carray(const _Carray&) noexcept;
 };
-
+/* Define if required, returns a std::istream& istream */
 template <typename Atype>
 inline _input_stream operator>>(_input_stream input, _Carray<Atype> const & object) noexcept
 { 
-  std::cout << "_Input_stream_returned\n";
   return input;
 }
-
+/* Define if required, returns a std::ostream& ostream */
 template <typename Atype>
 inline _output_stream operator<<(_output_stream output,_Carray<Atype> const & object) noexcept
 { 
-  output << "_Output_stream_returned\n";
   return output;
 }
-
+/* Return value @ array[index] */
 template <typename Atype>
-inline Atype &_Carray<Atype>::operator[](int i) noexcept
+inline Atype& _Carray<Atype>::operator[](int index) noexcept
 { 
-  std::cout << "_Accessed_\n" << i;
-  return array[i];
+  return array[index];
 }
-
+/*Increments each element of the array */
 template <typename Atype>
-inline _Carray<Atype>& _Carray<Atype>::operator++(int _dummy)
+inline _Carray<Atype> _Carray<Atype>::operator++(int _dummy)
 { 
-  std::cout << "_Increment_\n";
+  for(auto i = 0; i<BUFF_SIZE; i++){
+    array[i] = ++array[i];
+  }
+  return *this;
 }
-
+/* Decrements each element of the array */
 template <typename Atype>
-inline _Carray<Atype>& _Carray<Atype>::operator--(int _dummy)
+inline _Carray<Atype> _Carray<Atype>::operator--(int _dummy)
 { 
-  std::cout << "_Decrement_\n";
+  for(auto i = 0; i<BUFF_SIZE; i++){
+    array[i] = --array[i];
+  }
+  return *this;
 }
-
+/* Allocate an object instance from the heap with nothrow set */
 template <typename Atype>
-inline _void_ptr _Carray<Atype>::operator new (_size_type size, _nothrow nthrw)
+inline _void_ptr _Carray<Atype>::operator new(_size_type size, _nothrow nthrw)
 { 
   _void_ptr p = NULL;
   p = malloc(size);
   if(!p){
-    std::cout << "_Memory_Allocation returned NULL\n";
     exit(EXIT_FAILURE);
   }
-  std::cout << "_Memory_Allocated for object\n";
   return p;
 }
+/* Allocate an object instance from the heap */
 template <typename Atype>
-inline _void_ptr _Carray<Atype>::operator new (_size_type size)
+inline _void_ptr _Carray<Atype>::operator new(_size_type size)
 { 
   _void_ptr p = NULL;
   p = malloc(size);
   if(!p){
-    std::cout << "_Memory_Allocation returned NULL\n";
     exit(EXIT_FAILURE);
   }
-  std::cout << "_Memory_Allocated for object\n";
   return p;
 }
-
+/* Allocate from heap an array of objects with nothrow set */
 template <typename Atype>
-inline _void_ptr _Carray<Atype>::operator new[] (_size_type size, _nothrow nthrw)
+inline _void_ptr _Carray<Atype>::operator new[](_size_type size, _nothrow nthrw)
 { 
   _void_ptr p = NULL;
   p = malloc(size);
   if(!p){
-    std::cout << "_Memory_Allocation returned NULL\n";
     exit(EXIT_FAILURE);
   }
-  std::cout << "_Memory_Allocated for array of objects\n";
   return p;
 }
-
+/* Allocate from heap an array of objects */
 template <typename Atype>
-inline _void_ptr _Carray<Atype>::operator new[] (_size_type size)
+inline _void_ptr _Carray<Atype>::operator new[](_size_type size)
 { 
   _void_ptr p = NULL;
   p = malloc(size);
   if(!p){
-    std::cout << "_Memory_Allocation returned NULL\n";
     exit(EXIT_FAILURE);
   }
-  std::cout << "_Memory_Allocated for array of objects\n";
   return p;
 }
-
+/* Delete objects */
 template <typename Atype>
 inline void _Carray<Atype>::operator delete(_void_ptr p)
 {
-  std::cout << "_Memory_Deallocated\n";
   free(p);
 }
-
+/* Delete objects with nothrow set */
 template <typename Atype>
 inline void _Carray<Atype>::operator delete(_void_ptr p,_nothrow nthrw)
 {
-  std::cout << "_Memory_Deallocated\n";
   free(p);
 }
-
+/* Delete array of objects */
 template <typename Atype>
 inline void _Carray<Atype>::operator delete[](_void_ptr p)
 {
-  std::cout << "_Memory_Deallocated for array of objects\n";
   free(p);
 }
-
+/* Delete array of objects with nothrow set */
 template <typename Atype>
 inline void _Carray<Atype>::operator delete[](_void_ptr p,_nothrow nthrw)
 {
-  std::cout << "_Memory_Deallocated for array of objects\n";
   free(p);
 }
-
+/* Overloaded assignment operator */
 template <typename Atype>
-inline _Carray<Atype>& _Carray<Atype>::operator=( _Carray<Atype>& object)
+inline _Carray<Atype> _Carray<Atype>::operator=( _Carray<Atype>& object)
 {
-  std::cout << "_Shadow_Assignment Ocurred\n";
+  array = new Atype[BUFF_SIZE];
+  if(!array){exit(EXIT_FAILURE);}
+  for(auto i = 0; i <= BUFF_SIZE; i++){
+    array[i] = object.array[i];
+  }
+  size = object.size;
+  iterator = object.iterator;
+  _alloc_ = object._alloc_;
+  return *this;
 }
-
+/* Will return the object with offset of array values as passed */
 template <typename Atype>
-inline _Carray<Atype> _Carray<Atype>::operator()( _Carray<Atype>& object,_int_type i)
+inline _Carray<Atype> _Carray<Atype>::operator()(int num)
 {
-  std::cout << "_Functor_Access Ocurred\n";
+  for(auto i = 0; i <= BUFF_SIZE; i++)
+  {
+    this->array[i] = this->array[i] + num;
+  }
+  return *this;
 }
-
+/* Copy Constructor for object copy & move */
 template <typename Atype>
 inline _Carray<Atype>::_Carray(const _Carray& object) noexcept
 {
-  std::cout << "_Copy_Constructor Called\n";
+  array = new Atype[BUFF_SIZE];
+  if(!array){exit(EXIT_FAILURE);}
+  for(auto i = 0; i <= BUFF_SIZE; i++){
+    array[i] = object.array[i];
+  }
+  size = object.size;
+  iterator = object.iterator;
+  _alloc_ = object._alloc_;
 }
-
+/* Default constructor with initialization */
 template <typename Atype>
 inline _Carray<Atype>::_Carray() noexcept
 {
-  std::cout << "_Default_Constructor Called\n";
+  array = new Atype[BUFF_SIZE];
+  if(!array){exit(EXIT_FAILURE);}
+  for(auto i = 0; i <= BUFF_SIZE; i++){
+    array[i] = 0;
+  }
+  size = 512;
+  iterator = nullptr;
+  _alloc_ = nullptr;
 }
-
+/* Destructor for deleting instance */
 template <typename Atype>
 inline _Carray<Atype>::~_Carray() noexcept
 {
-  std::cout << "_Destructor Called\n";
+  // Deallocate memory that was previously reserved for array;
+  if(array){
+    delete[] array;
+  } 
 }
-
+/* Parameter size is passed to ctor */
 template <typename Atype>
 inline _Carray<Atype>::_Carray(_size_type size) : size(size)
 {
-  std::cout << "_Parameterized_Constructor Called\n";
+  array = new Atype[BUFF_SIZE];
+  if(!array){exit(EXIT_FAILURE);}
+  for(auto i = 0; i <= BUFF_SIZE; i++){
+    array[i] = 0;
+  }
+  iterator = nullptr;
+  _alloc_ = nullptr;
 }
+#endif
