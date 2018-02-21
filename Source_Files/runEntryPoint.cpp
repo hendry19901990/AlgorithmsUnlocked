0c1e09f165abefe1bdc2d38e519524af4a9e0fe5
SpawnTree <shekharhiran@hotmail.com>
2018-02-21 17:13:54 +0530

    run Commit

--- Source_Files/runEntryPoint.cpp
+++ Source_Files/runEntryPoint.cpp
@@ -1,12 +1,38 @@
+#include <bits/stdc++.h>
+#include <thread>
+#include <vector>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <array>
+#include <map>
+#include <list>
+#include <deque>
+#include <bitset>
+#include <queue>
+#include <forward_list>
+#include <set>
+#include <algorithm>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
 #include "libstringfunc.h"
 #include "stringPointers.h"
 #include "structPointers.h"
+#define itn int
 #define MAX_LIMIT_SZ 2048
+#ifndef WINDOWS_LIB_H
+    inline int pidorand() {
+        return ((rand() & 32767 << 16) | (rand() & 32767));
+    }
+    #define rand pidorand
+#endif
 using namespace std;
 int main(int argc, char const **argv)
 {   
