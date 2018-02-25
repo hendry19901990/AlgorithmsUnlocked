54dc2cd70b53c70a2392d9d708e46fbad6ba6a04
CodeObsess <SpawnTree@users.noreply.github.com>
2018-02-25 20:52:03 +0530

    git update

--- Source_Files/libfileop.cpp
+++ Source_Files/libfileop.cpp
@@ -7,35 +7,33 @@ static int count = 0;
 #ifndef FILE 
 #define FILE 0
 #endif
+/* A dangerous way of file handling */
 int main(int argc, char** argv)
 {
     /* File Object */
     ifstream ifile;
     ofstream ofile;
     /* User input data */
-    string data, modified_data;
+    string data, modified_data, ch = "Y";
     /* Store the input data into a stream */
     stringstream instream; 
-    ofile.open("dump.bin", ios::binary | ios::out | ios::in  | ios::app);
+    ofile.open("dump.bin", ios::out | ios::in  | ios::app);
     if(ofile.fail())
     {
         cout << "No file found...trying to create a new file" << endl;
-        ofile.open("dump.bin", ios::binary | ios::out | ios::in);
+        ofile.open("dump.bin", ios::out | ios::in);
     }
-    cout << "Enter the data to be saved  : " ;
-    getline(cin, data);
-    ofile << data << endl;
-    /*  Explicit read 
-    ifile >> modified_data;
-      Explicitly using the file to read and count the words in a string stream 
-    instream << modified_data;
-    while(instream >> data)
-    {
-        modified_data = modified_data + " " +  data;
-        ::count++;
-    }
-    modified_data = modified_data + "$ is what you entered and the word count is : ";
-    file << modified_data << ::count;
-    file.close(); */
+    do{
+        cout << "Enter the data to be saved  : " ;
+        getline(cin, data);
+        ofile << endl << data;
+        cout << endl << "Want to enter more (Y / N) : " << endl;
+        getline(cin,ch);
+    }while(ch == "y" || ch == "Y");
+    /* See what I just did */
+    ifile.open("dump.bin", ios::in);
+    while(ifile >> ch)  {  ::count++;  }
+    ofile << endl << "Word Count now " <<  ::count;
+    ofile.close();
     return 0;
 }
