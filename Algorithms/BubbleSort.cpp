baa8ef4ad0cdd52f8650749aea3e1fa3cdbbcecd
SpawnTree <shekharhiran@hotmail.com>
2018-03-23 09:46:47 +0530

    Updates

--- Algorithms/BubbleSort.cpp
+++ Algorithms/BubbleSort.cpp
@@ -7,11 +7,34 @@
 #include <vector>
 #include <map>
 using namespace std;
-
-
-
-
+void print(const int* A)
+{
+    int n = sizeof(A) / sizeof( A[0] );
+    for(auto i = 0; i < 11; i++)
+    {
+        cout << A[i] << "," ;
+    }
+    cout << endl;
+}
+void bubblesort(int* A)
+{
+    int n = sizeof(A) / sizeof( A[0] );
+    for(auto i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for(auto j = 0; j < n-i-1; j++)
+        {
+            if(A[j] > A[j + 1])
+            std::swap(A[j], A[j + 1]);
+            swapped = true;
+        }
+        if(swapped == false) break;
+    }
+}
 int main()
 {
-
+    int array[] = {3,6,7,5,7,8,9,4,1,2,1};
+    bubblesort(array);
+    print(array);
+    return 0;
 }
--- Algorithms/SelectionSort.cpp
+++ Algorithms/SelectionSort.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits>
 #include <string.h>
-#include <math.h>
+#include <algorithm>
 #include <vector>
-#include <map>
 using namespace std;
-
-
-
-
+void recursiveselsort(int* A, size_t n)
+{
+    if(n <= 1) return;
+    recursiveselsort(A, n-1);
+    int min = 0;
+    for(auto i = 0; i < n; i++)
+    {
+        if(A[i] < A[min])
+        {
+            min = i;
+        }
+        swap(A[min], A[0]);
+    }
+}
+void print(int* A, size_t n)
+{
+    for(auto j = 0; j < n; j++)
+    {
+        cout << A[j] << " " ;
+    }
+    cout << endl;
+}
 int main()
 {
-
+    int A[] = {5,54,56,58,41,56,25,85,74,56,32,14,785,41,478,541,458,874};
+    recursiveselsort(A, 18);
+    print(A, 18);
+    return 0;
 }
