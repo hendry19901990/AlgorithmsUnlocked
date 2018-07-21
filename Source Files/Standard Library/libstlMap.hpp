066e6af77fbeed6d406c1f3023b5fd670ec1aa03
SpawnTree <shekharhiran@hotmail.com>
2018-07-21 15:31:26 +0530

    Unordered Map
    Updated.

--- Source Files/Standard Library/libstlMap.hpp
+++ Source Files/Standard Library/libstlMap.hpp
@@ -1,56 +1,49 @@
 #include <iostream>
+#include <string>
+#include <memory>
 #include <stdio.h>
-#include <vector>
-#include <list>
-#include <algorithm>
-#include <numeric>
-#include <set>
-#include <map>
+#include <regex>
 #include <sstream>
-#include <cstdio>
-#include <string.h>
-#include <activation.h>
-#define sz(a) int((a).size()) 
-#define pb push_back 
-#define all(c) (c).begin(), (c).end()
-#define present(c,x) ((c).find(x) != (c).end()) 
-#define cpresent(c,x) (find(all(c),x) != (c).end()) 
-#define tr(container, it) for(decltype(container.begin()) it = container.begin(); it != container.end(); it++)
+#include <ctype.h>
+#include <unordered_map>
+#include <map>
 using namespace std;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> ii;
-void reverse_Array(int *A, int N)
-{
-	int *first = A;
-	int *last = A + N - 1;
-	while (first < last)
+int main()
+{	
+	unordered_map<int,string> mymap;
+	unordered_map<int,string>::hasher hash_func = mymap.hash_function();
+	mymap[9] = "Sumit Lahiri";
+	mymap[14] = "Shuvradeep Lahiri";
+	mymap[86] = "Google";
+	mymap[23] = "Pradipta Lahiri";
+	mymap[35] = "Pradipta Lahiri";
+	mymap[49] = "Susovan Lahiri";
+	mymap[52] = "Google";
+	mymap[56] = "Microsoft";
+	mymap[245] = "Apple";
+	mymap[127] = "HackerEarth";
+	mymap[364] = "HackerRank";
+	mymap[1255] = "HackerRank";
+	mymap[732] = "Microsoft";
+	mymap.emplace (1701, "J.T. Kirk");
+	mymap.emplace (18654, "Rowan Atkinson");
+	unsigned size = mymap.bucket_count();
+	for(const auto& x : mymap)
 	{
-		swap(A[*first], A[*last]);
-		first++; // Move the first index forward;
-		last--; //Move the last index back;
+		std::cout << "Element [" << x.first << " : " << x.second << "]";
+    	std::cout << " is in bucket #" << mymap.bucket(x.first) << std::endl;
 	}
-}
-template <typename T>
-void reverser(T& A, size_t N)
-{
-	T* f = A;
-	T* l = A + N - 1;
-	while (f < l)
-	{
-		swap(A[*f], A[*l]);
-		f++;
-		l--;
-	}
-
-}
-void f(const vector<int>& v) {
-	int r = 0;
-	tr(v, it) {
-		r += (*it)*(*it);
-	}
-}
-void mapfunc()
-{
-		
+   	for (unsigned i = 0; i < size; ++i)
+   	{
+    	std::cout << "bucket #" << i << " has " << mymap.bucket_size(i) << " elements.\n";
+  	}
+  	std::cout << "mymap's buckets contain:\n";
+  	for (auto i = 0; i < mymap.bucket_count(); ++i)
+  	{
+    	std::cout << "bucket #" << i << " contains:";
+    	for (auto start = mymap.begin(i); start != mymap.end(i); ++start)
+    		std::cout << "{" << start->first << " : " << start->second << "}  ";
+    	std::cout << std::endl;
+  	}
+	return 0;
 }
